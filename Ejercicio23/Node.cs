ad7a5f68ec17ca159b7e62232c6b023e4a5e6b6e
Erick Hernández <134026061+Erickhu-11@users.noreply.github.com>
2023-08-16 12:28:43 -0600

    Ejercicio 2

---
+++ Ejercicio23/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio23
+{
+    public class Node
+    {
+        public int Valor { get; set; }
+        public Node? Siguiente { get; set; }
+
+        public Node(int value)
+        {
+            Valor = value;
+            Siguiente = null;
+        }
+    }
+
+}
---
+++ Ejercicio23/Program.cs
@@ -0,0 +1,83 @@
+﻿using Ejercicio23;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        LinkedListCompleter listCompleter = new LinkedListCompleter();
+
+
+        Console.Write("Ingrese el valor de inicio: ");
+        int valorInicial = int.Parse(Console.ReadLine());
+
+
+        Console.Write("Ingrese el valor final: ");
+        int valorFinal = int.Parse(Console.ReadLine());
+
+
+        listCompleter.Agregar(valorInicial);
+            
+        listCompleter.CompleteList(valorFinal);
+
+        Console.WriteLine("Lista completa:");
+        listCompleter.PrintList();
+    }
+}
+
+public class LinkedListCompleter
+{
+    public Node Cabeza { get; set; }
+
+    public LinkedListCompleter()
+    {
+        Cabeza = null;
+    }
+
+    public void Agregar(int value)
+    {
+        if (Cabeza == null)
+        {
+            Cabeza = new Node(value);
+        }
+        else
+        {
+            Node actual = Cabeza;
+            while (actual.Siguiente != null)
+            {
+                actual = actual.Siguiente;
+            }
+            actual.Siguiente = new Node(value);
+        }
+    }
+
+    public void CompleteList(int valorFinal)
+    {
+        Node actual = Cabeza;
+        int valorSiguiente = Cabeza.Valor + 1;
+
+        while (valorSiguiente <= valorFinal)
+        {
+            if (actual.Siguiente == null || actual.Siguiente.Valor > valorSiguiente)
+            {
+                Node newNode = new Node(valorSiguiente);
+                newNode.Siguiente = actual.Siguiente;
+                actual.Siguiente = newNode;
+            }
+            actual = actual.Siguiente;
+            valorSiguiente++;
+        }
+    }
+
+    public void PrintList()
+    {
+        Node actual = Cabeza;
+        while (actual != null)
+        {
+            Console.Write(actual.Valor + " ");
+            actual = actual.Siguiente;
+        }
+        Console.WriteLine();
+    }
+}
